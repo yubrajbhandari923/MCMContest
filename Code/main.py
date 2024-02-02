f6e40d864c36cc3a33104f72d011e6189a67ef51
Kaden McLaughlin <kaden.mclaughlin@duke.edu>
2024-02-01 22:09:41 -0500

    more code

--- Code/main.py
+++ Code/main.py
@@ -3,5 +3,7 @@ from read import *
 
 
 
-rows = read_csv('./MCMContest/Data/Wimbledon_featured_matches.csv')
+# rows = read_csv('./MCMContest/Data/Wimbledon_featured_matches.csv')
+
+rows = read_csv('./MCMContest/Data/game.csv')
 
